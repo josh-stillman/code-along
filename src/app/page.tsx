8f160339f0a6be97ed02d3c17a23469b732773bb
Josh Stillman <josh.stillman@giantmachines.com>
2023-08-14 14:25:18 -0400

    fix lint errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image';
 import styles from './page.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+        <h1>
+          Hello World we are&nbsp;
+          <code className={styles.code}>deployed!!</code>
+        </h1>
+        <Link href="/foobar">go to foobar page!</Link>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
